3d5833fe0aafa149eaa4d8b6cf7901e081fe7182
2019100265N <2019100265N@psu.palawan.edu.ph>
2022-09-11 10:52:50 +0800

    inc directory done

---
+++ cover-verbose-memory/contacts.php
@@ -0,0 +1,29 @@
+<?php include 'inc/header.php'; ?>
+
+<!-- Custom styles for this template -->
+<link href="cover.css" rel="stylesheet">
+</head>
+
+<body class="text-center">
+
+<div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+  <header class="masthead mb-auto">
+    <div class="inner">
+      <h3 class="masthead-brand">Underdogs</h3>
+      <nav class="nav nav-masthead justify-content-center">
+        <a class="nav-link" href="index.php">Home</a>
+        <a class="nav-link" href="features.php">Features</a>
+        <a class="nav-link active" href="contacts.php">Contact</a>
+      </nav>
+    </div>
+  </header>
+
+  <main role="main" class="inner cover">
+    <h1 class="cover-heading">Contact Us</h1>
+    <p class="lead">For inquiries, you may contact us at our hotline: (476) 670-2386</p>
+    <p class="lead">
+      <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+    </p>
+  </main>
+
+  <?php include 'inc/footer.php'; ?>
---
+++ cover-verbose-memory/index.php
@@ -0,0 +1,29 @@
+<?php include 'inc/header.php'; ?>
+
+<!-- Custom styles for this template -->
+<link href="cover.css" rel="stylesheet">
+</head>
+
+<body class="text-center">
+
+<div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+  <header class="masthead mb-auto">
+    <div class="inner">
+      <h3 class="masthead-brand">Underdogs</h3>
+      <nav class="nav nav-masthead justify-content-center">
+        <a class="nav-link active" href="index.php">Home</a>
+        <a class="nav-link" href="features.php">Features</a>
+        <a class="nav-link" href="contacts.php">Contact</a>
+      </nav>
+    </div>
+  </header>
+
+  <main role="main" class="inner cover">
+    <h1 class="cover-heading">Welcome</h1>
+    <p class="lead">Welcome to Underdogs, sign up now to enjoy our various services and exciting new offers!</p>
+    <p class="lead">
+      <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+    </p>
+  </main>
+
+  <?php include 'inc/footer.php'; ?>
